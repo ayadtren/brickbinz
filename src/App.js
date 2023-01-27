4dbb6fb1f8bea65569ff099e1a9111685649ae94
Jjuicetin <puppies4648@gmail.com>
2023-01-27 13:47:19 -0700

    ?

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import './App.css';
-import Hello from './components/Hello'
 import Message from './components/message';
 import Navbar from './components/Navbar';
 function App() {
