3f3ee40bce64cddcb3cb0553009c9ce080cc66b9
ayadtren <88941128+ayadtren@users.noreply.github.com>
2023-01-31 16:16:35 -0700

    changed shoplego

--- src/pages/ShopLegos.js
+++ src/pages/ShopLegos.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react'
-import Navbar from '../components/Navbar'
+import Navbar from '../components'
+
 export class ShopLegos extends Component {
   render() {
     return (
       <div>
        
         <h1>inventory </h1>
-        <p3>use classes for CSS (.elementName use dots) and use ids for super specific element. (#specificElementName use hashtags)</p3>
       </div>
     )
   }
 }
+
+
+const ProductList = ({ products }) => {
+  return (
+    <ul>
+      {products.map(product => (
+        <li key={product.id}>
+          <h3>{product.name}</h3>
+          <p>{product.description}</p>
+          <p>Price: {product.price}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default ShopLegos
