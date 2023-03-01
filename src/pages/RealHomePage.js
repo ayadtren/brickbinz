a812e2a8286255b8287944b3079a010eb740c15f
Xyrille Tugade <xyrilletugads@gmail.com>
2023-02-28 20:31:47 -0700

    Added fetching data code for homepage

--- src/pages/RealHomePage.js
+++ src/pages/RealHomePage.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import picture from "../images/mainpagepic.jpg";
 import sampleimage from '../images/sampleimage.jpg'
 import Card from '../components/Card';
 import './HomePageStyles.scss'
 import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from "axios";
 //import {Carousel} from 'react-bootstrap';
 //<img className='main-image' src={picture} alt="Main picture" /> 
 const RealHomePage = () => {
+  
+  //Fetch Data Code; to use, products.map(('any parameter') => {enter format with values}) to fetch the code. Refer to Shop.js Line 167 for example of usage.
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchAllProducts = async () => {
+      try {
+        const res = await axios.get("http://localhost:8000/products");
+        setProducts(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchAllProducts();
+  }, []);
+  
   return (
     <>
         <div className='main-image-container'>
