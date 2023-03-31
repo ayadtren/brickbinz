b7b2815d9326bb1f202fa55ede5c766257a82129
Allan To <113399719+AllanToo@users.noreply.github.com>
2023-03-31 15:25:55 -0600

    revert to previous version

--- src/pages/Bookevent.js
+++ src/pages/Bookevent.js
@@ -1,104 +1,116 @@
-import React, { useState } from 'react';
-import './Event.scss';
-import axios from 'axios';
+import React from 'react';
+import "./Event.scss"
 
-const BookEvent = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [date, setDate] = useState('');
-  const [time, setTime] = useState('');
-  const [numberOfGuests, setNumberOfGuests] = useState('');
-  const [description, setDescription] = useState('');
+class Bookevent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            date: '',
+            time: '',
+            numberOfGuests: '',
+            submitted: false,
+        };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const newEvent = {
-      event_user_name: name,
-      event_email: email,
-      event_date: date,
-      event_time: time,
-      event_number_guest: numberOfGuests,
-      event_description: description
-    };
-
-    try {
-      await axios.post('http://localhost:8000/event', newEvent);
-      alert('The event has been added to db');
-    } catch (err) {
-      console.log(err);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-  };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Name:
-        <input
-          placeholder="Enter your name"
-          required
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </label>
-      <br />
-      <label>
-        Email:
-        <input
-          placeholder="Enter your Email"
-          required
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </label>
-      <br />
-      <label>
-        Date:
-        <input
-          type="date"
-          required
-          value={date}
-          onChange={(e) => setDate(e.target.value)}
-        />
-      </label>
-      <br />
-      <label>
-        Time:
-        <input
-          type="time"
-          required
-          value={time}
-          onChange={(e) => setTime(e.target.value)}
-        />
-      </label>
-      <br />
-      <label>
-        Number of guests:
-        <input
-          placeholder="Enter number of guests"
-          required
-          type="number"
-          value={numberOfGuests}
-          onChange={(e) => setNumberOfGuests(e.target.value)}
-        />
-      </label>
-      <br/>
-      Event Description
-        <input
-          placeholder="Enter Desc"
-          required
-          type="text"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-      <br />
-      <button className="add-event" type="submit">
-        Book Event
-      </button>
-    </form>
-  );
-};
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
 
-export default BookEvent;
+    handleSubmit(event) {
+        this.setState({
+            submitted: true
+        });
+        event.preventDefault();
+    }
+
+    renderForm() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Name:
+                    <input placeholder="Enter your name" required 
+                        type="text"
+                        value={this.state.name}
+                        name="name"
+                        onChange={this.handleChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    Email:
+                    <input placeholder="Enter your Email" required
+                        type="email"
+                        value={this.state.email}
+                        name="email"
+                        onChange={this.handleChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    Date:
+                    <input
+                        type="date" required
+                        value={this.state.date}
+                        name="date"
+                        onChange={this.handleChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    Time: 
+                    <input
+                        type="time" required
+                        value={this.state.time}
+                        name="time"
+                        onChange={this.handleChange}
+                    />
+                </label>
+                <br />
+                <label> 
+                    Number of guests: 
+                    <input placeholder="Enter number of guests" required
+                        type="number"
+                        value={this.state.numberOfGuests}
+                        name="numberOfGuests"
+                        onChange={this.handleChange}
+                    />
+                </label>
+                <br />
+                <input type="submit" value="Book" />
+            </form>
+        );
+    }
+
+    renderConfirmation() {
+        return (
+            <div className='center'>
+                <h2>Thank you for your booking!</h2>
+                <p>Name: {this.state.name}</p>
+                <p>Email: {this.state.email}</p>
+                <p>Date: {this.state.date}</p>
+                <p>Time: {this.state.time}</p>
+                <p>Number of guests: {this.state.numberOfGuests}</p>
+            </div>
+        );
+    }
+
+    render() {
+        return this.state.submitted ? (
+            this.renderConfirmation()
+        ) : (
+            <div>
+                <h1>Book your Lego Store Birthday Party Event</h1>
+                {this.renderForm()}
+            </div>
+        );
+    }
+}
+
+export default Bookevent;
