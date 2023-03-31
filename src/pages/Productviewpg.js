c4772bab108d961a66bacc7976f2bc21a2b59ac6
manaveoad <manave.oad@edu.sait.ca>
2023-03-31 13:14:21 -0600

    changes made

--- src/pages/Productviewpg.js
+++ src/pages/Productviewpg.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./Productviewpg.scss";
-
+import sampleimage from '../images/sampleimage.jpg'
 const Productviewpg = () => {
   return (
     <div className="group-parent">
-      <img className="frame-child" alt="" src="/group-6.svg" />
+      <img className="frame-child" alt="" src={sampleimage} />
       <div className="info-container">
         <div className="product-title">Product Title:</div>
         <div className="price">Price:</div>
@@ -12,10 +12,10 @@ const Productviewpg = () => {
           <p className="Description">Description:</p>
         </div>
       <div className="image-icons-container">
-        <img className="image-1-icon " alt="" src="" />
-        <img className="image-2-icon " alt="" src="" />
-        <img className="image-3-icon " alt="" src="" />
-        <img className="image-4-icon " alt="" src="" />
+        <img className="image-1-icon " alt="" src={sampleimage} />
+        <img className="image-2-icon " alt="" src={sampleimage}/>
+        <img className="image-3-icon " alt="" src={sampleimage}/>
+        <img className="image-4-icon " alt="" src={sampleimage} />
       </div>
     </div>
   );
