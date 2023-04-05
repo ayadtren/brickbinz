c38b3e2c6166104dafb1a4ca2bfef2d372680c3b
Justin Gil <puppies4648@gmail.com>
2023-04-05 15:29:03 -0600

    checkout page

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import '../styles/CheckoutStyles.scss'
 import axios from 'axios';
 const Checkout = () => {
+
+    //get data from cart.
     const [cartItems, setCartItems] = useState([]);
     useEffect(() => {
         const fetchAllCartItems = async () => {
@@ -15,11 +17,21 @@ const Checkout = () => {
         fetchAllCartItems();
     }, []);
 
+    //displays product name and price.
+    const listItems = cartItems.map((cartItems) => (
+        <div className='display-cart'>
+
+            <li key={cartItems.cart_set_numb}>
+                <h5>{cartItems.cart_set_name}</h5>
+                <p>${cartItems.cart_set_price}</p>
+            </li>
+            {/* hr is just line break. */}
+            <hr></hr>
+        </div>
+    ));
     return (
         <div>
-            {/* we need 5 div containers. 2 for the titles, 
-      2 for the contents such as cart summary and payment form.
-      and the other one for the button. */}
+
 
             {/*maybe relative positioning can be used...*/}
             <div className='payment-container'>
@@ -27,27 +39,35 @@ const Checkout = () => {
                     <h2>Payment Options</h2>
                 </div>
                 <div className='payment-form'>
-                    <h5>gang gnag</h5>
-                    radom text
-                    <p>hsuehehekjekjeh</p>
+                    Brand: <input type="text" placeholder="Visa" name="setName" required />
+                    Card Number: <input type="text" placeholder="Card Number" name="setPrice" required />
+                    Expiry Date: <input type="text" placeholder="MM / YY" name="setLocation" required />
+                    Card Holder: <input type="text" placeholder="Card Holder name" name="setQuantity" required />
+                    CVV: <input type='number' placeholder="CVV" name="setImage" required />
+                    Billing Postal Code: <input type="text" placeholder="A0A 0A0" name="postalCode" required />
                 </div>
 
             </div>
-            <div className='summary-container'>
+            <div className='checkout-summary-container'>
                 <div className='summary-title'>
                     <h2>Cart Summary</h2><p>edit link</p>
-
-                    <div className='summary-list'>
-
-                    </div>
                 </div>
-            </div>
+                <div className='summary-list'>
 
+                    <ul> {listItems}</ul>
+
+                </div>
+                <div className='summary-total-price'>
+                    gang gnag brouh
+                </div>
+
+            </div>
+           
             <div className="button">
-                    <button className="checkout-button" >
-                      Place Order
-                    </button>
-                  </div>
+                <button className="checkout-button" >
+                    Place Order
+                </button>
+            </div>
         </div>
     )
 }
