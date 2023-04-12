80697f4638e82074c96c5373da6f853d0f5e1274
Xyrille Tugade <xyrilletugads@gmail.com>
2023-04-11 23:52:45 -0600

    Changes

--- src/pages/checkout/Review.js
+++ src/pages/checkout/Review.js
@@ -5,51 +5,49 @@ import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import Grid from "@mui/material/Grid";
 
-const products = [
-  {
-    name: "Product 1",
-    desc: "A nice thing",
-    price: "$9.99",
-  },
-  {
-    name: "Product 2",
-    desc: "Another thing",
-    price: "$3.45",
-  },
-  {
-    name: "Product 3",
-    desc: "Something else",
-    price: "$6.51",
-  },
-  {
-    name: "Product 4",
-    desc: "Best thing of all",
-    price: "$14.11",
-  },
-  { name: "Shipping", desc: "", price: "Free" },
-];
+// const products = [
+//   {
+//     name: "Product 1",
+//     desc: "A nice thing",
+//     price: "$9.99",
+//   },
+//   {
+//     name: "Product 2",
+//     desc: "Another thing",
+//     price: "$3.45",
+//   },
+//   {
+//     name: "Product 3",
+//     desc: "Something else",
+//     price: "$6.51",
+//   },
+//   {
+//     name: "Product 4",
+//     desc: "Best thing of all",
+//     price: "$14.11",
+//   },
+//   { name: "Shipping", desc: "", price: "Free" },
+// ];
 
-const addresses = ["1 MUI Drive", "Reactville", "Anytown", "99999", "USA"];
-const payments = [
-  { name: "Card type", detail: "Visa" },
-  { name: "Card holder", detail: "Mr John Smith" },
-  { name: "Card number", detail: "xxxx-xxxx-xxxx-1234" },
-  { name: "Expiry date", detail: "04/2024" },
-];
+// const addresses = ["1 MUI Drive", "Reactville", "Anytown", "99999", "USA"];
+// const payments = [
+//   { name: "Card type", detail: "Visa" },
+//   { name: "Card holder", detail: "Mr John Smith" },
+//   { name: "Card number", detail: "xxxx-xxxx-xxxx-1234" },
+//   { name: "Expiry date", detail: "04/2024" },
+// ];
 
-function Review() {
-  
-  
+function Review({ userInfo, paymentInfo, cartItems }) {
   return (
     <React.Fragment>
       <Typography variant="h6" gutterBottom>
         Order summary
       </Typography>
       <List disablePadding>
-        {products.map((product) => (
-          <ListItem key={product.name} sx={{ py: 1, px: 0 }}>
-            <ListItemText primary={product.name} secondary={product.desc} />
-            <Typography variant="body2">{product.price}</Typography>
+        {cartItems.map((cartItem) => (
+          <ListItem key={cartItem.cart_set_numb} sx={{ py: 1, px: 0 }}>
+            <ListItemText primary={cartItem.cart_set_name} secondary={cartItem.cart_set_numb} />
+            <Typography variant="body2">{cartItem.cart_set_price}</Typography>
           </ListItem>
         ))}
 
@@ -61,25 +59,27 @@ function Review() {
         </ListItem>
       </List>
       <Grid container spacing={2}>
-        <Grid item xs={12} sm={6}>
-          <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
-            Shipping
-          </Typography>
-          <Typography gutterBottom>John Smith</Typography>
-          <Typography gutterBottom>{addresses.join(", ")}</Typography>
-        </Grid>
+        {userInfo.map((user) => (
+          <Grid item xs={12} sm={6} key={user.firstName}>
+            <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
+              Contact Information
+            </Typography>
+            <Typography gutterBottom>{user.firstName} {user.lastName}</Typography>
+            <Typography gutterBottom>{user.email}</Typography>
+          </Grid>
+        ))}
         <Grid item container direction="column" xs={12} sm={6}>
           <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
             Payment details
           </Typography>
           <Grid container>
-            {payments.map((payment) => (
-              <React.Fragment key={payment.name}>
+            {paymentInfo.map((payment) => (
+              <React.Fragment key={payment.cardName}>
                 <Grid item xs={6}>
-                  <Typography gutterBottom>{payment.name}</Typography>
+                  <Typography gutterBottom>{payment.cardNum}</Typography>
                 </Grid>
                 <Grid item xs={6}>
-                  <Typography gutterBottom>{payment.detail}</Typography>
+                  <Typography gutterBottom>{payment.cardExp}</Typography>
                 </Grid>
               </React.Fragment>
             ))}
