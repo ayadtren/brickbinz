21b6bc5710ea0c2b4bc34114fd4de5ee85d99abe
ayadtren <88941128+ayadtren@users.noreply.github.com>
2023-04-12 00:23:03 -0600

    product detail pop up

--- src/pages/Productviewpg.js
+++ src/pages/Productviewpg.js
@@ -1,30 +1,54 @@
 import React from "react";
-import "../styles/Productviewpg.scss";
-import sampleimage from '../images/sampleimage.jpg'
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  Button,
+} from "@mui/material";
+import { styled } from "@mui/system";
+import sampleimage from "../images/sampleimage.jpg";
+
+const StyledCard = styled(Card)({
+  maxWidth: 345,
+  margin: "auto",
+  position: "relative",
+});
+
+const StyledMedia = styled(CardMedia)({
+  height: 140,
+});
+
+const ActionButtons = styled("div")({
+  position: "absolute",
+  bottom: 16,
+  right: 16,
+});
+
 const Productviewpg = () => {
   return (
-    <div className="group-parent">
-      <img className="frame-child" alt="" src="" />
-      <div className="info-container">
-        <div className="product-title">Product Title:</div>
-        <div className="price">Price:</div>
-        <div className="store-location-nw">Store Location: </div>
-          <p className="Description">Description:</p>
-        </div>
-      <div className="image-icons-container">
-        <img className="image-1-icon " alt="" src="" />
-        <img className="image-2-icon " alt="" src="{sampleimage}"/>
-        <img className="image-3-icon " alt="" src=""/>
-        <img className="image-4-icon " alt="" src="" />
-      </div>
-      <div className="RemoveButton-div">
-      <a href="http://localhost:3001/"> <button className="RemoveButton" >X</button> </a>
-      </div>
-
-      <div className="SubmitButton-div">
-      <button type="submit" className="AddTocart">Add to cart</button>
-      </div> 
-    </div>
+    <StyledCard>
+      <StyledMedia image={sampleimage} title="Sample Product" />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Product Title
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Price: $10
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Store Location: Calgary
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Description: This is a sample product description.
+        </Typography>
+      </CardContent>
+      <ActionButtons>
+        <Button variant="contained" color="primary">
+          Add to cart
+        </Button>
+      </ActionButtons>
+    </StyledCard>
   );
 };
 
